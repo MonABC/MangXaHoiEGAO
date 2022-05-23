da29b03aaa1306062d49ff206cf7dc33e946900b
MonABC <maitienvietdhcn@gmail.com>
2022-05-23 15:44:03 +0700

    done

---
+++ src/app/notification-add-friend/notification-add-friend.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { NotificationAddFriendRoutingModule } from './notification-add-friend-routing.module';
+import { NotificationAddFriendListComponent } from './notification-add-friend-list/notification-add-friend-list.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ScrollingModule} from '@angular/cdk/scrolling';
+import {NgbDropdownModule} from '@ng-bootstrap/ng-bootstrap';
+
+
+@NgModule({
+  declarations: [NotificationAddFriendListComponent, NavBarComponent],
+  imports: [
+    CommonModule,
+    NotificationAddFriendRoutingModule,
+    ReactiveFormsModule,
+    ScrollingModule,
+    NgbDropdownModule
+  ]
+})
+export class NotificationAddFriendModule { }
